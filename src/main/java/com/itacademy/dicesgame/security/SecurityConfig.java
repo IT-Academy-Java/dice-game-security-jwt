d93bce0fd48be2a4eb55954f1799c166ee3eadd6
Elena Piaggio <elenapiaggio@gmail.com>
2021-01-18 18:55:28 +0100

    ADD configuration global and permit all urls /players

--- src/main/java/com/itacademy/dicesgame/security/SecurityConfig.java
+++ src/main/java/com/itacademy/dicesgame/security/SecurityConfig.java
@@ -1,43 +1,44 @@
 package com.itacademy.dicesgame.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        PasswordEncoder encoder = passwordEncoder();
+        UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+
         auth.inMemoryAuthentication()
-                .withUser("admin")
-                    .password("{noop}123")
-                    .roles("ADMIN", "USER")
-                .and()
-                .withUser("user")
-                    .password("{noop}123")
-                    .roles("USER");
+                .withUser(users.username("admin").password("123").roles("ADMIN", "USER"))
+                .withUser(users.username("elena").password("123").roles("USER"));
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable().formLogin().disable().authorizeRequests()
-                .antMatchers("/players").permitAll()
+                //.csrf().disable().formLogin().disable().authorizeRequests()
+                //.antMatchers("/players").permitAll()
 
-
-                .antMatchers(HttpMethod.GET).permitAll()
-                .antMatchers(HttpMethod.POST).hasAnyRole("ADMIN", "USER")
-                .antMatchers(HttpMethod.PUT).hasRole("ADMIN")
-
-                .antMatchers("/players/")
-                    .hasAnyRole("ADMIN");
-
-
-
+            .authorizeRequests().antMatchers("/players").permitAll();
     }
 
 
