4fcb6692fed3d10ed405d89c892b103d9a56ac91
Elena Piaggio <elenapiaggio@gmail.com>
2021-08-31 15:01:19 +0200

    ADD jwt implementation

--- src/main/java/com/itacademy/dicesgame/DicesgameApplication.java
+++ src/main/java/com/itacademy/dicesgame/DicesgameApplication.java
@@ -1,13 +1,38 @@
 package com.itacademy.dicesgame;
 
+import com.itacademy.dicesgame.security.JWTAuthorizationFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @SpringBootApplication
 public class DicesgameApplication {
 
 	 public static void main(String[] args) {
 		SpringApplication.run(DicesgameApplication.class, args);
+	}
+
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	 	@Override
+		protected void configure(HttpSecurity http) throws Exception{
+	 		http
+					.csrf().disable()
+					.addFilterAfter(new JWTAuthorizationFilter(),
+							UsernamePasswordAuthenticationFilter.class)
+					.authorizeRequests()
+					.antMatchers(HttpMethod.POST, "/user").permitAll()
+					.antMatchers(HttpMethod.GET, "/players").permitAll()
+					.anyRequest().permitAll();
+
+		}
 	}
 
 }
--- src/main/java/com/itacademy/dicesgame/entity/User.java
+++ src/main/java/com/itacademy/dicesgame/entity/User.java
@@ -22,4 +22,20 @@ public class User {
     public void setUser(String user) {
         this.user = user;
     }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
